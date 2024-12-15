a8e12cafb9d12ecfd2638979bb725b2e60593712
Aliya Beyonce Almaden <aliyabeyonce.almaden@tup.edu.ph>
2024-12-15 12:01:05 +0800

    sumarry

--- admin_login.php
+++ admin_login.php
@@ -20,6 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     } else {
         $error_message = "Invalid username or password.";
+        echo $error_message;
     }
 
     $stmt->close();
